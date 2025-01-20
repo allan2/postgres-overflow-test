58f290dd35102b4210000f131cfd18a9344eda19
Allan <6740989+allan2@users.noreply.github.com>
2025-01-20 01:07:12 -0500

    Check more types

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,45 @@
 use chrono::NaiveDateTime;
-use postgres::{Config, NoTls};
+use postgres::{Client, Config, NoTls};
 use time::PrimitiveDateTime;
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut config = Config::new();
     config.host("/var/run/postgresql");
     let mut client = config.connect(NoTls)?;
 
-    let row = client.query_one(
-        "SELECT 'infinity'::timestamp",
-        &[],
-    )?;
+    // OK
+    inf_test(&mut client, "date")?;
+
+    // panics
+    inf_test(&mut client, "timestamp")?;
+    inf_test(&mut client, "timestamptz")?;
+
+    Ok(())
+}
+
+
+fn inf_test(client: &mut Client, ty: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let query = format!("SELECT 'infinity'::{ty}");
+
+    println!("Testing {ty}");
+
+    let row = client.query_one(&query, &[])?;
+
+    println!("chrono");
 
     // `chrono` overflow handled
-    let chrono_err =  row.try_get::<_, NaiveDateTime>(0).unwrap_err();
-    eprintln!("{chrono_err}");
+    let chrono_err = row.try_get::<_, NaiveDateTime>(0).unwrap_err();
+    eprintln!("{ty} with chrono: {chrono_err}");
+
+    println!("time");
 
     // `time` overflow panics
-    match row.try_get::<_, PrimitiveDateTime>(0) {  // this panics
+    match row.try_get::<_, PrimitiveDateTime>(0) {
         Ok(_) => unreachable!(),
-        Err(e) => println!("Error: {}", e),
+        Err(e) => println!("{ty} with time: Error: {e}"),
     }
+
+    println!("\n");
     Ok(())
 }
